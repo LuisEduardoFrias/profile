aa3acd7c68ed1c8bbde855295e480e5fe30eeaa4
luis eduardo frias <luiseduardofrias27@gmail.com>
2024-03-30 00:46:04 -0400

    dividi los estilos por archivo, y trabaje con el footer

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -1,64 +1,32 @@
-/** @format */
 
+import { ReactElement } from "react"
 import Image from "next/image";
-import localFont from "next/font/local";
-
-type Option = {
-	src: string;
-	alt: string;
-	name: string;
-};
-
-function Li({ src, alt, name }: { src: string; alt: string; name: string }) {
-	return (
-		<li>
-			<a>
-				<Image src={src} width={24} height={24} alt={alt} />
-				<p className='tooltip'>{name}</p>
-			</a>
-		</li>
-	);
-}
+import Li, { Option } from "cp/li.tsx";
+import MenuList from "public/jsons/manu_list.json"
+import Socials from "public/jsons/socials.json"
+import "st/footer.css";
 
 export default function Footer({ days_one }: { days_one: any }) {
-	//
-	const lis: Option[] = [
-		{
-			src: "/skills2.png",
-			alt: "Skills icon",
-			name: "Skill"
-		},
-		{
-			src: "/projects.png",
-			alt: "Projects icon",
-			name: "Projects"
-		},
-		{
-			src: "/skills2.png",
-			alt: "Skills icon",
-			name: "Skill"
-		},
-		{
-			src: "/skills2.png",
-			alt: "Skills icon",
-			name: "Skill"
-		},
-		{
-			src: "/skills2.png",
-			alt: "Skills icon",
-			name: "Skill"
-		}
-	];
-
-	return (
-		<footer>
-			<nav className={days_one.className}>
-				<ul>
-					{lis.map((li: Option, i: number) => (
-						<Li key={i} {...li} />
-					))}
-				</ul>
-			</nav>
-		</footer>
-	);
+    //
+    return (
+        <footer className="footer-container" >
+            <nav className={days_one.className}>
+                <ul>
+                    {MenuList.map((li: Option, i: number) => <Li key={i} {...li} />)}
+                </ul>
+            </nav>
+            <div>
+                <button>
+                    <Image src="/images/cv.png" width={25} height={25} alt={" icon"} />
+                </button>
+                <div>
+                    {Socials.map((social: object, i: number) =>
+                        <a href={social.href}>
+                            <Image src={social.img} width={25} height={25} alt={social.alt} />
+                        </a >)
+                    }
+                </div>
+            </div>
+        </footer >
+    );
 }
--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,20 +1,21 @@
 /** @format */
 
 import localFont from "next/font/local";
+import "st/header.css";
 
 export default function Header({ days_one }: { days_one: any }) {
 	return (
-		<header>
+		<header className="header-container">
 			<div>
 				<div className={days_one.className}>
 					<h1>Hola! soy programador </h1>
 					<h2>BackEnd y FrontEnd</h2>
+					<div>
+						<p>
+							{"soy programador fullStack, me apasiona crear soliciones innovadora y eficientes para sastisfacer las necesidades de los usuairos."}
+						</p>
+					</div>
 				</div>
-				<p>
-					Mi nombre es Luis Eduardo y soy programador fullStack, me apasiona
-					crear soliciones innovadora y eficientes para sastisfacer las
-					necesidades de los usuairos.
-				</p>
 			</div>
 		</header>
 	);
