25c4b51a48e0d7c94e0bdd447688597591054a1e
luis eduardo frias <luiseduardofrias27@gmail.com>
2024-06-03 11:00:52 -0400

    realize un pull 3da vez

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "images/profile",
+    basePath: "/profile/images",
     output: "export",
     reactStrictMode: true,
 };
