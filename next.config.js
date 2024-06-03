3ee6c4819880e1ef331d19d5b8dd1fd7b478ef20
luis eduardo frias <luiseduardofrias27@gmail.com>
2024-06-03 09:46:56 -0400

    configuracion next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: "/2048-in-react",
+  output: "export",
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
