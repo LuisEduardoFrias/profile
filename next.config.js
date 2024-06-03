cc9b7d63d41577c05ac91f706d203c6abd59c35b
luis eduardo frias <luiseduardofrias27@gmail.com>
2024-06-03 10:47:33 -0400

    ruta para las imagenes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/2048-in-react",
-  output: "export",
-  reactStrictMode: true,
+    basePath: "/profile",
+    output: "export",
+    reactStrictMode: true,
 };
 
 module.exports = nextConfig;
